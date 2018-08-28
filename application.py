5720c8f4438f452f48ba90baf152f7e77f7a60f0
Andre Tonelli <aveigas@br.ibm.com>
2018-08-28 08:51:34 -0300

    Edit ship function

--- application.py
+++ application.py
@@ -29,16 +29,49 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-#login configurations here
-
-# main page route
+#Routes
+# main page
 
 @app.route('/')
 @app.route('/starships/')
-def showBooks():
+def starshipsCatalog():
     starships = session.query(Starships).all()
     return render_template('starships.html', starships=starships)
     
+#Create a newShip
+
+@app.route('/starships/new/', methods=['GET', 'POST'])
+def newShip():
+    if request.method == 'POST':
+        newShip = Starships(name=request.form['name'], description=request.form['description'], category=request.form['category'])
+        session.add(newShip)
+        session.commit()
+        flash("New starship added!")
+        return redirect(url_for('starshipsCatalog'))
+    else:
+        return render_template('newship.html')
+
+# Edit a ship
+
+@app.route('/starships/<int:ship_id>/edit', methods=['GET', 'POST'])
+def editShip(ship_id):
+    editedShip = session.query(Starships).filter_by(id=ship_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedShip.name = request.form['name']
+        if request.form['description']:
+            editedShip.description = request.form['description']
+        if request.form['category']:
+            editedShip.price = request.form['category']
+        session.add(editedShip)
+        session.commit()
+        flash("Starship edited!")
+        return redirect(url_for('starshipsCatalog'))
+    else:
+        return render_template('editship.html', ship_id=ship_id, ship=editedShip)
+
+    
 if __name__ == '__main__':
   app.debug = True
+  app.secret_key = 'super_secret_key'
   app.run(host = '0.0.0.0', port = 5000)
